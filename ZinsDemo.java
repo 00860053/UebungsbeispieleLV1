4a1ad9a23b57244a4362da24016462f5f2f7adf1
00860053 <00860053@edu.campus02.at>
2022-09-28 23:00:34 +0200

    Ausgabe mit unterschiedlichen Werten

--- ZinsDemo.java
+++ ZinsDemo.java
@@ -2,14 +2,13 @@ public class ZinsDemo {
     public static void main(String[] args) {
         //System.out.println("HelloWorld");
 
-        //Initialisierung
-        int kontostand = 1000;
-        double zinsen = 2.25;
-
-
         //Ausgabe
-        System.out.println("Kontostand: " + kontostand + "; Zinsen: " + zinsen);
-        System.out.println("Zinsen am Ende der Laufzeit = " + zinsrechnung(kontostand, zinsen));
+        //System.out.println("Kontostand: " + int kontostand = 1000; + "; Zinsen: " + double zinsen= 2.25);
+        System.out.println("Zinsen am Ende der Laufzeit = Kontostand: " + zinsrechnung(1000, 2.25));
+        System.out.println("Zinsen am Ende der Laufzeit = Kontostand: " + zinsrechnung(3000, 4.75));
+        System.out.println("Zinsen am Ende der Laufzeit = Kontostand: " + zinsrechnung(4759, 1.55));
+
+
 
     }
 
