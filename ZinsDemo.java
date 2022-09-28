b079f5ec27d49914fca414443ed73f6dccc42cec
00860053 <00860053@edu.campus02.at>
2022-09-28 22:43:53 +0200

    Ausagerung der Berechnung in eigene Methode

--- ZinsDemo.java
+++ ZinsDemo.java
@@ -6,11 +6,16 @@ public class ZinsDemo {
         int kontostand = 1000;
         double zinsen = 2.25;
 
-        //Operation
-        double zinsenLaufzeitende = zinsen * kontostand / 100;
 
         //Ausgabe
-        System.out.println("Zinsen am Ende der Laufzeit = " + zinsenLaufzeitende);
+        System.out.println("Kontostand: " + kontostand + "; Zinsen: " + zinsen);
+        System.out.println("Zinsen am Ende der Laufzeit = " + zinsrechnung(kontostand, zinsen));
+
+    }
+
+    public static double zinsrechnung (int kontostand, double zinsen) {
+        double zinsenLaufzeitende = zinsen * kontostand / 100;
+        return zinsenLaufzeitende;
 
     }
 }
