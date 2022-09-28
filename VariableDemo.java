0ea8239f8fc35fb4027b198d8ac06222d748b23f
00860053 <00860053@edu.campus02.at>
2022-09-28 21:59:33 +0200

    Beispiel 1 done

---
+++ VariableDemo.java
@@ -0,0 +1,12 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        //System.out.println("HelloWorld");
+
+        String varVorname;
+        varVorname = "Max";
+
+        String varNachname = "Mustermann";
+
+        System.out.println("Der Name lautet: " + varVorname + varNachname);
+    }
+}
